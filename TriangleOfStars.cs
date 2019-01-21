12499e5b7168a7939814af8e0543a3f8d335f837
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-21 08:48:19 +0200

    TriangleOfStars.cs

---
+++ TriangleOfStars.cs
@@ -0,0 +1,22 @@
+using System;
+
+class TriangleOfStars 
+{
+	public static void Main()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			for (int inter = 0; inter < 5 - i; inter += 1)
+			{
+				Console.Write(" ");
+			}
+
+			for (int stars = 0; stars < 2 * i + 1; stars += 1)
+			{
+				Console.Write("*");
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
